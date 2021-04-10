3ea247ff6184d95afa656af4d49d542e653f5ae9
LeoFu9487 <leofu890806@gmail.com>
2021-04-10 13:17:57 +0200

    norm ok, remove tmp

--- srcs/main.c
+++ srcs/main.c
@@ -6,17 +6,38 @@
 /*   By: yfu <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/09 20:59:29 by yfu               #+#    #+#             */
-/*   Updated: 2021/04/09 23:03:34 by yfu              ###   ########lyon.fr   */
+/*   Updated: 2021/04/10 13:16:48 by yfu              ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ofc.h"
 
+void	sub_solve(int ct[2], int **dp, int **map, int target[3])
+{
+	if (map[ct[0]][ct[1]] == map[ct[0] - 1][ct[1]]
+		&& map[ct[0]][ct[1]] == map[ct[0]][ct[1] - 1]
+		&& map[ct[0]][ct[1]] == map[ct[0] - 1][ct[1] - 1])
+		dp[ct[0]][ct[1]] = 1 + ft_min(dp[ct[0]][ct[1] - 1], ft_min(
+					dp[ct[0] - 1][ct[1]], dp[ct[0] - 1][ct[1] - 1]));
+	else
+		dp[ct[0]][ct[1]] = 1;
+	if (dp[ct[0]][ct[1]] > target[2])
+	{
+		target[2] = dp[ct[0]][ct[1]];
+		target[0] = ct[0];
+		target[1] = ct[1];
+	}
+}
+
 void	solve(int n, int **map, int target[3])
 {
-	int	dp[n][n];
+	int	**dp;
 	int	ct[2];
 
+	dp = malloc(n * sizeof(int *));
+	ct[0] = -1;
+	while (++ct[0] < n)
+		dp[ct[0]] = malloc(n * sizeof(int));
 	ct[0] = -1;
 	target[2] = 0;
 	while (++ct[0] < n)
@@ -29,19 +50,12 @@ void	solve(int n, int **map, int target[3])
 	{
 		ct[1] = 0;
 		while (++ct[1] < n)
-		{
-			if (map[ct[0]][ct[1]] == map[ct[0] - 1][ct[1]] && map[ct[0]][ct[1]] == map[ct[0]][ct[1] - 1] && map[ct[0]][ct[1]] == map[ct[0] - 1][ct[1] - 1])
-				dp[ct[0]][ct[1]] = ft_min(dp[ct[0]][ct[1] - 1], ft_min(dp[ct[0] - 1][ct[1]], dp[ct[0] - 1][ct[1] - 1])) + 1;
-			else
-				dp[ct[0]][ct[1]] = 1;
-			if (dp[ct[0]][ct[1]] > target[2]) // didn't consider when there are multiple answers
-			{
-				target[2] = dp[ct[0]][ct[1]];
-				target[0] = ct[0];
-				target[1] = ct[1];
-			}
-		}
+			sub_solve(ct, dp, map, target);
 	}
+	ct[0] = -1;
+	while (++ct[0] < n)
+		free(dp[ct[0]]);
+	free(dp);
 }
 
 int	main(void)
@@ -50,13 +64,14 @@ int	main(void)
 	char	c;
 	int		**map;
 	int		ct[3];
+
 	parse(&n, &c, &map);
 	if (n <= 0)
 	{
 		ft_putendl_fd("", 1);
 		return (0);
 	}
-	solve(n, map, ct);// ct[0] : target i, ct[1] : target j, ct[2] : max_square_size
+	solve(n, map, ct);
 	print_ans(n, c, map, ct);
 	ct[0] = -1;
 	while (++ct[0] < n)
--- srcs/parse.c
+++ srcs/parse.c
@@ -6,7 +6,7 @@
 /*   By: yfu <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/09 21:57:41 by yfu               #+#    #+#             */
-/*   Updated: 2021/04/10 12:32:58 by yfu              ###   ########lyon.fr   */
+/*   Updated: 2021/04/10 12:58:01 by yfu              ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,38 +29,46 @@ int	read_int(void)
 	return (ans);
 }
 
-void	get_line(int n, int *line) // protection of read ?
+void	get_line(int n, int *line)
 {
-	char	buffer[n];
+	char	*buffer;
 	int		ct;
 
+	buffer = malloc(n * sizeof(char));
 	while (1)
 	{
 		if (read(0, buffer, 1) != 1)
+		{
+			free(buffer);
 			return ;
+		}
 		if (buffer[0] != '\n')
 			break ;
 	}
 	line[0] = buffer[0] - '0';
 	if (read(0, buffer, n - 1) != n - 1)
+	{
+		free(buffer);
 		return ;
+	}
 	ct = 0;
 	while (++ct < n)
 		line[ct] = buffer[ct - 1] -'0';
+	free(buffer);
 }
 
 void	parse(int *n, char *c, int ***map)
 {
 	int		ct[2];
 
-	*n = read_int(); // what if no input ?
-	read(0, c, 1); // if longer than one byte ? If no byte ?
+	*n = read_int();
+	read(0, c, 1);
 	if (*n <= 0)
 		return ;
-	*map = malloc(*n * sizeof(int*)); // what if malloc Fail ?
+	*map = malloc(*n * sizeof(int *));
 	ct[0] = -1;
-		while (++ct[0] < *n)
-			map[0][ct[0]] = malloc(*n * sizeof(int));
+	while (++ct[0] < *n)
+		map[0][ct[0]] = malloc(*n * sizeof(int));
 	ct[0] = -1;
 	while (++ct[0] < *n)
 		get_line(*n, map[0][ct[0]]);
@@ -76,7 +84,8 @@ void	print_ans(int n, char c, int **map, int target[3])
 		ct[1] = -1;
 		while (++ct[1] < n)
 		{
-			if (ct[0] > target[0] - target[2] && ct[0] <= target[0] && ct[1] > target[1] - target[2] && ct[1] <= target[1])
+			if (ct[0] > target[0] - target[2] && ct[0] <= target[0]
+				&& ct[1] > target[1] - target[2] && ct[1] <= target[1])
 				ft_putchar_fd(c, 1);
 			else
 				ft_putnbr_fd(map[ct[0]][ct[1]], 1);
