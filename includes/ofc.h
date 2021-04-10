349bf8fa0850bb6d28423adc1e104ae4152d4dd0
LeoFu9487 <leofu890806@gmail.com>
2021-04-10 12:48:16 +0200

    libft ok, compile ok

--- includes/ofc.h
+++ includes/ofc.h
@@ -6,14 +6,13 @@
 /*   By: yfu <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/09 21:56:47 by yfu               #+#    #+#             */
-/*   Updated: 2021/04/09 23:03:46 by yfu              ###   ########lyon.fr   */
+/*   Updated: 2021/04/10 12:02:30 by yfu              ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef OFC_H
 # define OFC_H
 # include "../libft/libft.h"
-# include "../libft/get_next_line.h"
 
 void	parse(int *n, char *c, int ***map);
 void	solve(int n, int **map, int target[3]);
